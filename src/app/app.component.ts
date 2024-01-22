3366b1e6c3e71fe99e5ae75a325704a3e3afea3d
Raquel <rauqel@pollex.com.tw>
2024-01-22 23:20:58 +0800

    加入登入頁面與程式邏輯

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,33 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatInputModule, MatToolbarModule, ReactiveFormsModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Raquel-bakery';
+  lock = true;
+  form: FormGroup = this.fb.group({
+    account: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+
+  get account(): AbstractControl {
+    return this.form.controls['account'];
+  }
+
+  get password(): AbstractControl {
+    return this.form.controls['password'];
+  }
 }
