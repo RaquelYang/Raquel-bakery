ed445d42686982971793fa25042a13e5735080b3
Raquel <rauqel@pollex.com.tw>
2024-01-23 23:25:06 +0800

    新增 lazyload component 路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./login/login.component').then(mod => mod.LoginComponent)
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./layouts/main/main.component').then(mod => mod.MainComponent)
+  }
+];
